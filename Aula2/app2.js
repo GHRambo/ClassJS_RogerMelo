e8ef5f84a8e0be5874e233fc52b78276f0424a84
Gustavo Henrique Rambo <98855371+GHRambo@users.noreply.github.com>
2023-11-12 19:48:41 -0300

    update aula 2

---
+++ Aula2/app2.js
@@ -0,0 +1,8 @@
+// Metodos comuns de Strings
+const email = "laracroft@gmail.com" ;   
+
+// const lastIndexOfA = email.lastIndexOf("a");
+// const emailSlice = email.slice(0, 9)
+const emailReplace = email.replace("l", "y")
+
+console.log(emailReplace)
